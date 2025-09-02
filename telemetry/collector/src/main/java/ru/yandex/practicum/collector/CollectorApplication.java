110597c1381a2fdd6424bd5a93852ca40e4133e5
Al3x Ivanov <pro87@yandex.ru>
2025-09-02 17:31:44 +0400

    Test fixes

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
+@CommandScan(basePackages = "ru.yandex.practicum.collector")
 public class CollectorApplication {
 
     public static void main(String[] args) {
