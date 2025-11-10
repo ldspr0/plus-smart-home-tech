3a8077afd362e08c8dfb6958ecd6a519dafa2af7
Al3x Ivanov <pro87@yandex.ru>
2025-11-11 02:10:59 +0400

    Warehouse schema fix

--- commerce/warehouse/src/main/resources/schema.sql
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -1,15 +1,28 @@
-drop table if exists shopping_cart, shopping_cart_items;
+drop table if exists warehouse_product, bookings, booking_products;
 
-create table if not exists shopping_cart
+create table if not exists warehouse_product
 (
-    shopping_cart_id uuid default gen_random_uuid() primary key,
-    username varchar(255) not null,
-    active boolean not null
+    product_id uuid primary key,
+    quantity   integer,
+    fragile    boolean,
+    width      double precision not null,
+    height     double precision not null,
+    depth      double precision not null,
+    weight     double precision not null
 );
 
-create table if not exists shopping_cart_items
+create table if not exists bookings
 (
-    product_id uuid not null,
-    quantity integer,
-    cart_id uuid references shopping_cart (shopping_cart_id) on delete cascade
+    shopping_cart_id uuid primary key,
+    delivery_weight  double precision not null,
+    delivery_volume  double precision not null,
+    fragile          boolean          not null,
+    order_id         uuid
+);
+
+create table if not exists booking_products
+(
+    shopping_cart_id uuid references bookings (shopping_cart_id) on delete cascade primary key,
+    product_id       uuid not null,
+    quantity         integer
 );
